a822625beeabedc30ff193cb51557eadaac72413
Anthony <anthony@superscanner.nl>
2026-02-26 15:50:21 +0100

    initial attempt, ~37 seconds

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,23 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
+    private array $map = [];
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $inputFile = fopen($inputPath, 'r');
+        while (($line = fgets($inputFile)) !== false) {
+            preg_match('/https:\/\/stitcher\.io(?<path>[^,]+),(?<date>202[^T]+)/' ,$line, $match);
+            $curr = $this->map[$match['path']][$match['date']] ?? 0;
+            $this->map[$match['path']][$match['date']] = $curr + 1;
+        }
+        fclose($inputFile);
+
+        foreach ($this->map as &$line) {
+            ksort($line);
+        }
+
+        file_put_contents($outputPath, json_encode($this->map, JSON_PRETTY_PRINT));
     }
 }
