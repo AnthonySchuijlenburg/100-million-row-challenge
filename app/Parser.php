891ffa0b3e659687f2d73cf71372be4d0854e54d
Anthony <anthony@superscanner.nl>
2026-02-27 15:17:57 +0100

    third attempt, ~24 seconds

--- app/Parser.php
+++ app/Parser.php
@@ -4,22 +4,38 @@ namespace App;
 
 final class Parser
 {
+    private const int PREFIX_LENGTH = 25;
+    private const int DATE_LENGTH = 11;
+
     private array $map = [];
     public function parse(string $inputPath, string $outputPath): void
     {
         $inputFile = fopen($inputPath, 'r');
-        while (($line = fgets($inputFile)) !== false) {
-            $line = explode(',', $line);
-            $path = substr($line[0], 19);
-            $date = substr($line[1], 0, 10);
+        while (($line = fgets($inputFile, 128)) !== false) {
+            $commaPos = strpos($line, ',');
+            [$path, $date] = explode(
+                ',',
+                substr(
+                    $line,
+                    self::PREFIX_LENGTH,
+                    $commaPos + self::DATE_LENGTH - self::PREFIX_LENGTH
+                )
+            );
 
-            $curr = $this->map[$path][$date] ?? 0;
-            $this->map[$path][$date] = $curr + 1;
+            $path = '/blog/'.$path;
+            $date = str_replace('-', '', $date);
+            $this->map[$path][$date] = ($this->map[$path][$date] ?? 0) + 1;
         }
         fclose($inputFile);
 
         foreach ($this->map as &$line) {
             ksort($line);
+
+            $modified = [];
+            foreach ($line as $key => $value) {
+                $modified[substr($key, 0, 4) . '-' . substr($key, 4, 2) . '-' . substr($key, 6, 2)] = $value;
+            }
+            $line = $modified;
         }
 
         file_put_contents($outputPath, json_encode($this->map, JSON_PRETTY_PRINT));
